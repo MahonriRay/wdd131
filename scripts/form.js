fdf4045314b5a4cb6be590080ef2b9a552c2e971
Mahonri <ray22004@byui.edu>
2024-05-25 22:45:41 -0600

    Initial Commit

---
+++ scripts/form.js
@@ -0,0 +1,62 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "Flux Capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "Power Laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "Time Circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "Low Voltage Reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "Warp Equalizer",
+        averagerating: 5.0
+    }
+];
+
+const productName = document.querySelector('#product-name');
+const form = document.querySelector('form');
+const reviewCounter = document.querySelector('#reviewCount');
+
+let reviewCount = localStorage.getItem('reviewCount') || 0;
+
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = "Last Modified: " + 
+    new Date(document.lastModified).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }) + 
+    " " + 
+    new Date(document.lastModified).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
+
+
+if (productName) {
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.setAttribute("value", product.id);
+        option.textContent = product.name
+        
+        productName.appendChild(option);
+    });
+
+}
+
+if (form) {
+    formLocator.addEventListener("submit", () => {    
+        reviewCount++;
+        localStorage.setItem("reviewCount", reviewCount);    
+    });
+}
+
+if (reviewCount) {
+    reviewCount.innerHTML = `Total reviews submitted: ${reviewCount}`;
+}
